e6381eaea7eb35b85db867cf5b54a6efe3527bbe
Rowan Tran <rowanjames.tran@gmail.com>
2017-05-25 17:49:59 -0700

    Add dx and dy to MovingBall constructor

--- src/com/rowan/ballgame/MovingBall.java
+++ src/com/rowan/ballgame/MovingBall.java
@@ -3,11 +3,13 @@ package com.rowan.ballgame;
 import java.awt.*;
 
 abstract class MovingBall extends Sprite {
-    MovingBall(int x, int y) {
+    MovingBall(int x, int y, int dx, int dy) {
         width = 20;
         height = 20;
         this.x = x;
         this.y = y;
+        this.dx = dx;
+        this.dy = dy;
     }
 
     void updateSpeed() {}
