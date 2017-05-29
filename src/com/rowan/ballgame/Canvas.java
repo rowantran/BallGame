f04fcf803a0fe10a3671c8a62e4fd4bdb8cbdcc1
Rowan Tran <rowanjames.tran@gmail.com>
2017-05-29 12:09:56 -0700

    Reduce redundant code when registering key bindings

--- src/com/rowan/ballgame/Canvas.java
+++ src/com/rowan/ballgame/Canvas.java
@@ -4,15 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.List;
 
 class Canvas extends JPanel implements ActionListener {
-    private boolean menuActive;
-
-    private Timer timer;
-
     private PlayerBall player;
 
     private List<MovingBall> balls;
@@ -32,36 +26,23 @@ class Canvas extends JPanel implements ActionListener {
     }
 
     private void init() {
-        InputMap im = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-
-        im.put(KeyStroke.getKeyStroke("W"), "MOVE_UP");
-        getActionMap().put("MOVE_UP", new MoveAction("W"));
-
-        im.put(KeyStroke.getKeyStroke("released W"), "MOVE_UP_RELEASE");
-        getActionMap().put("MOVE_UP_RELEASE", new MoveAction("released W"));
-
-        im.put(KeyStroke.getKeyStroke("S"), "MOVE_DOWN");
-        getActionMap().put("MOVE_DOWN", new MoveAction("S"));
-
-        im.put(KeyStroke.getKeyStroke("released S"), "MOVE_DOWN_RELEASE");
-        getActionMap().put("MOVE_DOWN_RELEASE", new MoveAction("released S"));
-
-        im.put(KeyStroke.getKeyStroke("A"), "MOVE_LEFT");
-        getActionMap().put("MOVE_LEFT", new MoveAction("A"));
-
-        im.put(KeyStroke.getKeyStroke("released A"), "MOVE_LEFT_RELEASE");
-        getActionMap().put("MOVE_LEFT_RELEASE", new MoveAction("released A"));
-
-        im.put(KeyStroke.getKeyStroke("D"), "MOVE_RIGHT");
-        getActionMap().put("MOVE_RIGHT", new MoveAction("D"));
-
-        im.put(KeyStroke.getKeyStroke("released D"), "MOVE_RIGHT_RELEASE");
-        getActionMap().put("MOVE_RIGHT_RELEASE", new MoveAction("released D"));
+        registerKeyStrokes("W", "released W", "S", "released S", "A", "released A", "D", "released D");
 
         setFocusable(true);
         setDoubleBuffered(true);
-        timer = new Timer(Resources.FRAME_TIME, this);
+
+        Timer timer = new Timer(Resources.FRAME_TIME, this);
         timer.start();
+    }
+
+    void registerKeyStrokes(String... keyCodes) {
+        // Registers each keyCode defined in keyCodes using MoveAction to call PlayerBall.move(keyCode)
+        InputMap im = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+        for (String keyCode : keyCodes) {
+            im.put(KeyStroke.getKeyStroke(keyCode), keyCode);
+            getActionMap().put(keyCode, new MoveAction(keyCode));
+        }
     }
 
     void loadData(Level level) {
@@ -80,7 +61,6 @@ class Canvas extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        CardLayout layout = (CardLayout) wrapper.getLayout();
         JPanel current = new JPanel();
         for (Component comp : wrapper.getComponents()) {
             if (comp.isVisible()) {
@@ -88,7 +68,7 @@ class Canvas extends JPanel implements ActionListener {
             }
         }
 
-        if (current.getName() == wrapper.PLAY_STATE) {
+        if (current.getName().equals(wrapper.PLAY_STATE)) {
             for (Sprite s : walls) {
                 s.draw(g);
             }
@@ -124,7 +104,7 @@ class Canvas extends JPanel implements ActionListener {
             }
         }
 
-        if (current.getName() == wrapper.PLAY_STATE) {
+        if (current.getName().equals(wrapper.PLAY_STATE)) {
             player.update(Resources.FPS);
             for (Sprite s : balls) {
                 s.update(Resources.FPS);
